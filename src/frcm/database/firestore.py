e5aeacbff9ace8362f8ebd84efcdb2aa967de1cd
Maksim Ohvrill <589664@stud.hvl.no>
2024-04-24 01:07:17 +0200

    database update

--- src/frcm/database/firestore.py
+++ src/frcm/database/firestore.py
@@ -5,6 +5,7 @@ from google.cloud import firestore
 import json
 from decouple import config
 from datetime import datetime
+from google.cloud.firestore_v1.base_query import FieldFilter
 from frcm.datamodel.model import WeatherDataPoint, Location, WeatherData, Observations, Forecast
 
 def get_firestore_client():
@@ -32,12 +33,14 @@ def get_firestore_db():
     except Exception as e:
         print(f"Error getting Firestore database: {e}")
         return None
-
-def save_data_to_db(location, date, risks, src):
+    
+def save_data_to_db(location, date, weatherpoints, src):
     try:
         db = get_firestore_db()
         if db is not None:
-            doc_ref = db.collection('weather_data').document(date)
+            # Use the location as a document identifier
+            loc_ref = db.collection('weather_data').document(f"{location.latitude}_{location.longitude}")
+            doc_ref = loc_ref.collection('days').document(date)
             data = {
                 'observation_date': date,
                 'source': src,
@@ -46,42 +49,55 @@ def save_data_to_db(location, date, risks, src):
                 'weather_points': []
             }
             
-            for risk in risks:
-                fire_risk_data = {
-                    'date_time': risk.timestamp.isoformat(),
-                    'temperature': risk.temperature,
-                    'humidity': risk.humidity,
-                    'wind_speed': risk.wind_speed
+            for weatherpoint in weatherpoints:
+                weather_data = {
+                    'date_time': weatherpoint.timestamp.isoformat(),
+                    'temperature': weatherpoint.temperature,
+                    'humidity': weatherpoint.humidity,
+                    'wind_speed': weatherpoint.wind_speed
                 }
-                data['weather_points'].append(fire_risk_data)
+                data['weather_points'].append(weather_data)
             
             doc_ref.set(data)
             print("Data saved successfully")
     except Exception as e:
         print(f"Failed to save data: {e}")
 
-def get_date_weatherdata(date):
+
+def get_date_weatherdata(location, start_date, end_date):
     try:
         db = get_firestore_db()
         if db is not None:
-            doc_ref = db.collection('weather_data').document(date)
-            doc = doc_ref.get()
-            if doc.exists:
-                doc_dict = doc.to_dict()
-                location = Location(latitude=doc_dict['latitude'], longitude=doc_dict['longitude'])
-                weather_data_points = [WeatherDataPoint(
-                    temperature=dp['temperature'],
-                    humidity=dp['humidity'],
-                    wind_speed=dp['wind_speed'],
-                    timestamp=datetime.fromisoformat(dp['date_time'])
-                ) for dp in doc_dict['weather_points']]
-                observations = Observations(source=doc_dict['source'], location=location, data=weather_data_points)
-                return observations
+            loc_ref = db.collection('weather_data').document(f"{location.latitude}_{location.longitude}")
+            # Setting up the query to fetch documents within the date range
+            query = loc_ref.collection('days')
+            query = query.where(filter=FieldFilter('observation_date', '>=', start_date))
+            query = query.where(filter=FieldFilter('observation_date', '<=', end_date))
+            query = query.order_by('observation_date')
+            results = list(query.stream())  # Convert to list for processing
+
+            all_observations = []
+            if results:
+                for doc in results:
+                    doc_dict = doc.to_dict()
+                    weather_data_points = [WeatherDataPoint(
+                        temperature=dp['temperature'],
+                        humidity=dp['humidity'],
+                        wind_speed=dp['wind_speed'],
+                        timestamp=datetime.fromisoformat(dp['date_time'])
+                    ) for dp in doc_dict['weather_points']]
+                    observations = Observations(source=doc_dict['source'], location=location, data=weather_data_points)
+                    all_observations.append(observations)
+                return all_observations
             else:
-                print("No such document!")
+                print("No documents found within the specified date range.")
+                return None
+        else:
+            print("No such document!")
     except Exception as e:
-        print(f"Error fetching data for {date}: {e}")
+        print(f"Error fetching data for {location} from {start_date} to {end_date}: {e}")
     return None
+
 
 def delete_firerisk(date):
     try:
--- src/frcm/logic/bus_logic.py
+++ src/frcm/logic/bus_logic.py
@@ -21,54 +21,115 @@ class FireRiskAPI:
     def compute(self, wd: WeatherData) -> FireRiskPrediction:
         return frcm.FRC_service.compute.compute(wd)
     
-    def fetch_and_compute(self, location, start, end, manage_db=False):
-        """Fetch observations and forecast, create WeatherData, and compute prediction."""
-        time_now = datetime.now()
-        formatted_start_time = start.strftime('%d-%m-%Y')
-        formatted_time_now = end.strftime('%d-%m-%Y')
-        date_range = f'{formatted_start_time} - {formatted_time_now}'
+    def get_day_bounds(self, date):
+        start_day = datetime(date.year, date.month, date.day)
+        end_day = start_day + timedelta(days=1)
+        return start_day, end_day
 
-        if manage_db:
-            observations = self.manage_observations(location, date_range, start, end)
-        else:
-            observations = self.client.fetch_observations(location, start=start, end=end)
+    def fetch_and_check_data(self, location, start_time, time_now):
+        days_to_fetch = []
+        current_day = start_time
+        while current_day < time_now:
+            days_to_fetch.append(current_day)
+            current_day += timedelta(days=1)
+        existing_data, missing_dates = self.check_existing_data(location, days_to_fetch)
+        return days_to_fetch, existing_data, missing_dates
 
-        forecast = self.client.fetch_forecast(location, start, end)
-        wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
-        return self.compute(wd)
-    
-    def manage_observations(self, location, date_range, start, end):
-        """Manage fetching or retrieving observations from database or API."""
-        db_observations = get_date_weatherdata(date_range)
-        if db_observations:
-            print("Using data retrieved from the database.")
-            return db_observations
-        else:
-            observations = self.client.fetch_observations(location, start=start, end=end)
-            save_data_to_db(observations.location, date_range, observations.data, observations.source)
-            print("No existing data found. Fetching new data from FROST")
-            return observations
+    def check_existing_data(self, location, days_to_fetch):
+        start_day, _ = self.get_day_bounds(days_to_fetch[0])
+        _, end_day = self.get_day_bounds(days_to_fetch[-1])
+        all_db_data = get_date_weatherdata(location, start_day.strftime('%d-%m-%Y'), end_day.strftime('%d-%m-%Y'))
+        all_db_data = all_db_data if all_db_data is not None else []
+        data_by_date = {obs.data[0].timestamp.strftime('%d-%m-%Y'): obs for obs in all_db_data}
         
-    def compute_previous_days(self, location: Location, delta: timedelta):
+        existing_data = {}
+        missing_dates = []
+        for day in days_to_fetch:
+            start_day, _ = self.get_day_bounds(day)
+            formatted_date = start_day.strftime('%d-%m-%Y')
+            db_data = data_by_date.get(formatted_date)
+            if db_data and db_data.data:
+                existing_data[day] = db_data
+            else:
+                missing_dates.append(day)
+        return existing_data, missing_dates
+
+    def fetch_missing_data(self, location, missing_dates, existing_data):
+        fetched_data = {}
+        for day in missing_dates:
+            start_day, end_day = self.get_day_bounds(day)
+            observations = self.client.fetch_observations(location, start=start_day, end=end_day)
+            save_data_to_db(location, start_day.strftime('%d-%m-%Y'), observations.data, observations.source)
+            existing_data[day] = observations
+
+    def aggregate_observations(self, days_to_fetch, existing_data):
+        all_observations = []
+        combined_source = ""
+        combined_location = None
+        for day in days_to_fetch:
+            if day in existing_data:
+                combined_source = existing_data[day].source
+                combined_location = existing_data[day].location
+                all_observations.extend(existing_data[day].data)
+        if not all_observations:
+            raise Exception("No observation data available for processing.")
+        return Observations(source=combined_source, location=combined_location, data=all_observations)
+
+    def compute_previous_days(self, location: Location, delta: timedelta) -> FireRiskPrediction:
         time_now = datetime.now()
         start_time = time_now - delta
-        return self.fetch_and_compute(location, start_time, time_now, manage_db=True)
-
-    def compute_upcoming_days(self, location: Location, delta: timedelta):
+        days_to_fetch, existing_data, missing_dates = self.fetch_and_check_data(location, start_time, time_now)
+        self.fetch_missing_data(location, missing_dates, existing_data)
+        combined_observations = self.aggregate_observations(days_to_fetch, existing_data)
+        # as normal 
+        forecast = self.client.fetch_forecast(location, start_time, time_now)
+        wd = WeatherData(created=time_now, observations=combined_observations, forecast=forecast)
+        prediction = self.compute(wd)
+        return prediction
+    
+    
+    def compute_upcoming_days(self, location: Location, delta: timedelta) -> FireRiskPrediction: 
         time_now = datetime.now()
         end_time = time_now + delta
-        return self.fetch_and_compute(location, time_now, end_time)
+        observations = self.client.fetch_observations(location, start=time_now, end=end_time)
+        # as normal
+        forecast = self.client.fetch_forecast(location, time_now, end_time)
+        wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
+        prediction = self.compute(wd)
+        return prediction
 
-    def compute_specific_period(self, location: Location, start: datetime, end: datetime):
-        return self.fetch_and_compute(location, start, end)
+    def compute_specific_period(self, location: Location, start: datetime, end: datetime) -> FireRiskPrediction:
+        time_now = datetime.now()
+        observations = self.client.fetch_observations(location, start=start, end=time_now)
+        days_to_fetch, existing_data, missing_dates = self.fetch_and_check_data(location, start, time_now)
+        self.fetch_missing_data(location, missing_dates, existing_data)
+        
+        if start < time_now:
+            observations = self.aggregate_observations(days_to_fetch, existing_data)
 
-    def compute_after_start_date(self, location: Location, start: datetime, delta: timedelta):
+        # as normal
+        forecast = self.client.fetch_forecast(location, start, end)
+        wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
+        prediction = self.compute(wd)
+        return prediction 
+
+    def compute_after_start_date(self, location: Location, start: datetime, delta: timedelta) -> FireRiskPrediction:
+        time_now = datetime.now()
         end = start + delta
-        return self.fetch_and_compute(location, start, end)
+        observations = self.client.fetch_observations(location, start=start, end=end)
+        forecast = self.client.fetch_forecast(location, start, end)
+        wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
+        prediction = self.compute(wd)
+        return prediction
 
-    def compute_before_end_date(self, location: Location, end: datetime, delta: timedelta):
+    def compute_before_end_date(self, location: Location, end: datetime, delta: timedelta) -> FireRiskPrediction:
+        time_now = datetime.now()
         start = end - delta
-        return self.fetch_and_compute(location, start, end)
+        observations = self.client.fetch_observations(location, start=start, end=end)
+        forecast = self.client.fetch_forecast(location, start, end)
+        wd = WeatherData(created=time_now, observations=observations, forecast=forecast)
+        prediction = self.compute(wd)
+        return prediction
 
 
 met_extractor = METExtractor()
