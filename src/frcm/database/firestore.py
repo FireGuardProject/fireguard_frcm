f18340e9d05b5643a9cd8afbb346df38bdac9379
Viktor Gravdal <Viktorkg98@gmail.com>
2024-04-26 12:52:49 +0200

    la inn print paa firestore.py feilsoking

--- src/frcm/database/firestore.py
+++ src/frcm/database/firestore.py
@@ -11,6 +11,7 @@ from frcm.datamodel.model import WeatherDataPoint, Location, WeatherData, Observ
 def get_firestore_client():
     try:
         # Decode the base64-encoded credentials
+        
         print("Credentials:")
         print(config('FIREBASE_CREDENTIALS_BASE64'))
         credentials_json = base64.b64decode(config('FIREBASE_CREDENTIALS_BASE64'))
